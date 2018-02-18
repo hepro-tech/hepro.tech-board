43dda468a51a0e3668cef6bf5475f69dfdc619ca
Ryan Huard <ryan.huard@live.ca>
2018-02-17 19:15:24 -0700

    Interrupt when spaghet has been touched

--- src/heprotechboard.ino
+++ src/heprotechboard.ino
@@ -1,17 +1,43 @@
 #include "cellular_hal.h"
 #include "proximity.h"
 #include "stdio.h"
+#include "application.h"
+
 // Set the TELUS APN
-  STARTUP(cellular_credentials_set("isp.telus.com", "", "", NULL));
-  Proximity_Sensor *prox;
-void setup() {
-  // Set the keep-alive value for TELUS SIM card
-  Particle.keepAlive(30);
-  prox = new Proximity_Sensor(B3);
+STARTUP(cellular_credentials_set("isp.telus.com", "", "", NULL));
+
+//Globals
+Proximity_Sensor *prox;
+volatile bool spaghetTouched = false;
+void  BEAM_ISR() {
+    spaghetTouched = true;
 }
+
+void setup() {
+    // Set the keep-alive value for TELUS SIM card
+
+    //Delay for 1 minute to allow proximity sensor to come online
+    Particle.publish("status","Initializing",60,PRIVATE);
+    Particle.keepAlive(30);
+    prox = new Proximity_Sensor(B3);
+
+    Serial.begin(9600);
+    delay(PROX_SENS_STARTUP_DELAY);
+
+     if (attachInterrupt(B3, BEAM_ISR, RISING)) {
+        Serial.println ("Interrupt attached to B3.");
+    } else {
+        Serial.println ("WARNING: couldn't attach interrupt to B3.");
+    }
+    Particle.publish("status","Start sensing",60,PRIVATE);
+}
+
 void loop () {
-  char digi[30] = {0 };
-  snprintf(digi, 30, "%d", prox->ReadDigital());
-  Particle.publish("beamStatus",digi,60,PRIVATE);
-  delay(1000);
+    if(spaghetTouched)
+    {
+      Particle.publish("MotionTracking", "SOMEBODY TOUCHA MY SPAGHET", 60, PRIVATE);
+      spaghetTouched = false;
+    }
+    delay(1000);
+
 }
--- src/proximity.h
+++ src/proximity.h
@@ -1,7 +1,7 @@
 #ifndef PROXIMITY_HH
 #define PROXIMITY_HH
 
-
+#define PROX_SENS_STARTUP_DELAY 60000
 class Proximity_Sensor {
     private:
         int pin;
